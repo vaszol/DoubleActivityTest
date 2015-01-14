06730efbb6d3b9d92b0fcb1ecc6bcda4b979da74
vas <vaszol@mail.ru>
2015-01-14 12:40:08 +0300

    работа с намерением android переключиться на вторую активность

---
+++ app/src/main/java/com/example/vas/doubleactivitytest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.vas.doubleactivitytest;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void showOther(View v) {
+        startActivity(new Intent(this, OtherActivity.class));
+    }
+}
