f6ccc74fc088ad607edb7250ed9ba1daf930e7dc
vas <vaszol@mail.ru>
2015-01-14 13:04:19 +0300

    Отображение места на карте по координатам переданным через intent

--- app/src/main/java/com/example/vas/doubleactivitytest/MainActivity.java
+++ app/src/main/java/com/example/vas/doubleactivitytest/MainActivity.java
@@ -1,23 +1,31 @@
 package com.example.vas.doubleactivitytest;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+    private EditText lat;
+    private EditText lon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lat= (EditText) findViewById(R.id.lat);
+        lon= (EditText) findViewById(R.id.lon);
     }
 
-
-    public void showOther(View v) {
-        startActivity(new Intent(this, OtherActivity.class));
+    public void showMe(View view){
+        String _lat=lat.getText().toString();
+        String _lon=lon.getText().toString();
+        Uri uri =Uri.parse("geo:"+_lat+","+_lon);
+        startActivity(new Intent(Intent.ACTION_VIEW, uri));
     }
+
 }
